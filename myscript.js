1187e534cc0d582f3e11c9d0d722345bf420aa91
Andy Kuang <akuang@mit.edu>
2017-01-13 13:31:43 -0500

    Initial HTML and CSS and JS

---
+++ myscript.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	//Materialize.toast('Welcome!!', 4000);
+	$('.grey-text').mouseenter(function(){
+		$(this).css("text-decoration", "underline");
+	});
+	$('.grey-text').mouseleave(function(){
+		$(this).css("text-decoration", "none");
+	});
+
+	$('#abouttext').mouseenter(function(){
+		$(this).css("text-decoration", "underline");
+	});
+	$('#abouttext').mouseleave(function(){
+		$(this).css("text-decoration", "none");
+	});
+	$('.parallax').parallax();
+});
