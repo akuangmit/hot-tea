cb9081100bd3a3c6046c49d0f112cc29b2c3b9d9
Rhea Lin <rhealin@mit.edu>
2017-01-18 18:07:57 -0500

    tried to add modal but it's kinda failing rn; will try to fix later

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -33,11 +33,10 @@ $(document).ready(function(){
         $('select').material_select();
     });
     
-     // $('.modal').modal();
-     // $('.modal-trigger').leanModal();
-
-     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    //$('.modal').modal();
+    $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
 
     Materialize.updateTextFields();
 
