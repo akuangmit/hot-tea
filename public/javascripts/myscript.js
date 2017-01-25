a7c8276c4b004ea1dadea53ea99fc4c1f2471516
Rhea Lin <rhealin@mit.edu>
2017-01-25 16:09:01 -0500

    navbar resizing, searchModal

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -102,6 +102,8 @@ $(document).ready(function(){
 		});
 	}
 
+	$(".button-collapse").sideNav();
+
 	$('select').material_select();
 
 	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
