d6bc903f73337f6b9c201e246bcd5ebe3ecb3ac1
Andy Kuang <akuang@mit.edu>
2017-01-29 20:58:12 -0500

    Fixed colon for bar graphs

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -306,7 +306,7 @@ $(document).ready(function(){
 			        "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm",
 			        "7 pm", "8 pm", "9 pm", "10 pm", "11 pm"],
 			        datasets: [{
-			        	label: "",
+			        	label: "Minutes",
 			            data: dataPoints,
 			            backgroundColor: [
 			                'rgba(255, 99, 132, 0.2)',
